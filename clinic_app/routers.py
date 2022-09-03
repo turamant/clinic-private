9802742d7313062f58adcb4a19f6c568378737e8
askvart <tur1amant@gmail.com>
2022-09-04 00:14:46 +0300

    routers/index()2

--- clinic_app/routers.py
+++ clinic_app/routers.py
@@ -7,7 +7,6 @@ from clinic_app import app, Appointment, Doctor, db
 def index():
     appointments = db.session.query(Appointment.data_appointment, Doctor.family).select_from(Appointment)\
         .join(Doctor).filter(Appointment.doctor_id == Doctor.id).all()
-    print("...appointments...=", appointments)
     return render_template('index.html', appointments=appointments)
 
 
