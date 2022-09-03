ceafec5e8083b62b8f5c33b9b14a491a69f18bd9
askvart <tur1amant@gmail.com>
2022-09-04 00:13:44 +0300

    routers/index()

--- clinic_app/routers.py
+++ clinic_app/routers.py
@@ -5,14 +5,6 @@ from clinic_app import app, Appointment, Doctor, db
 
 @app.route('/')
 def index():
-    '''
-    reviews = db.session.query(Reservation.room_id, Room.address, func.avg(Review.rating)).select_from(Reservation) \
-        .join(Review).filter(Review.reservation_id == Reservation.id) \
-        .join(Room).filter(Room.id == Reservation.room_id) \
-        .group_by(Reservation.room_id).having(func.avg(Review.rating)).all() '''
-
-    ''' select data_appointment, doctors.family from doctors_patients JOIN doctors
-    ON doctors_patients.id = doctors.id; '''
     appointments = db.session.query(Appointment.data_appointment, Doctor.family).select_from(Appointment)\
         .join(Doctor).filter(Appointment.doctor_id == Doctor.id).all()
     print("...appointments...=", appointments)
